2e977bd175f632ae8e3f9b580a62d29751693517
Crisanto L. Once Jr <once.crisanto.jr@gmail.com>
2026-08-23 20:30:33 +0800

    My pages

---
+++ app/contactus/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function ContactUs() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Welcome to the blog page!
+    </div>
+  );
+}
